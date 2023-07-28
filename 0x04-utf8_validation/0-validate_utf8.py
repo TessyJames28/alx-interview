9338a81f923d8df7dae7ac324798dd128d5a1695
TessyJames28 <tessyjames28@gmail.com>
2023-07-28 13:10:01 +0100

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,20 @@
 
 def validUTF8(data):
     """return true or false if utf-8 validated"""
-    x = "01111111"
-    if not data:
-        return False
-    for i in data:
-        if bin(i)[2:].zfill(8) > x:
-            return False
-        return True
+    byte = 0
+
+    for bits in data:
+        if byte == 0:
+            if bits >> 5 == 0b110 or bits >> 5 == 0b1110:
+                byte += 1
+            elif bits >> 4 == 0b1110:
+                byte += 2
+            elif bits >> 3 == 0b11110:
+                byte += 3
+            elif bits >> 7 == 0b1:
+                return False
+        else:
+            if bits >> 6 != 0b10:
+                return False
+            byte -= 1
+    return byte == 0
